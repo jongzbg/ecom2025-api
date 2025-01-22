ebaf56d6d11a309ba5913cdbd177900da4d028ed
Jong <jongzbg@gmail.com>
2025-01-22 12:02:46 +0700

    env

--- server.js
+++ server.js
@@ -12,11 +12,7 @@ const cors = require('cors')
 // middleware
 app.use(morgan('dev'))
 app.use(express.json({limit:'20mb'}))
-app.use(cors({
-    origin: 'https://ecom2025-nine.vercel.app',
-    credentials: true,
-  }));
-// app.use(cors())
+app.use(cors())
 // Step 3 Router
 // comment authRouter,categoryRouter because using readdirSync
 // app.post('/api', (req, res) => {
