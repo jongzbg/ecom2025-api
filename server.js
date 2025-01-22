2e22faf3540abafd818d02679ae4a175eb20a352
Jong <jongzbg@gmail.com>
2025-01-22 15:29:16 +0700

    server

--- server.js
+++ server.js
@@ -36,3 +36,7 @@ readdirSync('./routes')
 app.listen('5001', () => {
     console.log(`Server is running on port 5001`)
 })
+
+app.get('/',(req,res)=>{
+    res.send('This is my API running...')
+})
